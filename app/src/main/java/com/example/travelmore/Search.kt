d9f2852cf4b9b27348ca2872fa90dde2efee6a55
Ashley Plasket <ashley.plasket@gmail.com>
2023-11-18 16:00:42 -0500

    remove placeClient from searchbox function, update imports

--- app/src/main/java/com/example/travelmore/Search.kt
+++ app/src/main/java/com/example/travelmore/Search.kt
@@ -1,30 +1,33 @@
 package com.example.travelmore
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.libraries.places.api.net.PlacesClient
 import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
 fun SearchBox(
     modifier: Modifier = Modifier,
-    searchIcon: ImageVector = Icons.Default.Search,
-    placesClient: PlacesClient // Inject the PlacesClient dependency
-) {
+
+
+    ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(Color.LightGray)
             .padding(horizontal = 16.dp, vertical = 8.dp),
 
-    ) {
+        ) {
 
         Spacer(modifier = Modifier.width(8.dp))
         BasicTextField(
