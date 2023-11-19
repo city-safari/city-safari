b8c555563d3dd4c1bd1dd2a4471728d1875b9160
Sarah Garlock <garlock.sarah10@gmail.com>
2023-11-18 20:16:22 -0700

    add maps plugin and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,11 @@
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
 
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
